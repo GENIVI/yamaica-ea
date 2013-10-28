baed2403fbd5a0a3504f867d5da05a559defd873
Johannes Langlotz <johannes.langlotz@partner.bmw.de>
2013-10-28 11:59:19 +0100

    Added test plug-in for EA tests.

---
+++ tests/de.bmw.yamaica.ea.tests/src/de/bmw/yamaica/ea/tests/AllTests.java
@@ -0,0 +1,20 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package de.bmw.yamaica.ea.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.bmw.yamaica.ea.tests.positive.PerformanceTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PerformanceTest.class })
+public class AllTests
+{
+
+}
---
+++ tests/de.bmw.yamaica.ea.tests/src/de/bmw/yamaica/ea/tests/positive/PerformanceTest.java
@@ -0,0 +1,44 @@
+/* Copyright (C) 2013 BMW Group
+ * Author: Manfred Bathelt (manfred.bathelt@bmw.de)
+ * Author: Juergen Gehring (juergen.gehring@bmw.de)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package de.bmw.yamaica.ea.tests.positive;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PerformanceTest
+{
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+}
